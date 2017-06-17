0c21d751e2d84cb0a1ba2c1aed8dd453a11f68f1
myhero105 <proteinlaboratory2@gmail.com>
2017-06-17 18:13:36 -0400

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,27 @@
+
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+shinyUI(fluidPage(
+    titlePanel("Regression Models in iris dataset"),
+    sidebarLayout(
+        sidebarPanel(
+            h3("Slope"),
+            textOutput("slopeOut"),
+            h3("Intercept"),
+            textOutput("intOut")
+        ),
+        mainPanel(
+            plotOutput("plot1", brush = brushOpts(
+                id = "brush1"
+            ))  # THe users can choose any data points to make linear regression model
+        )
+    )
+))
